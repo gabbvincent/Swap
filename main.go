7dab21a6ca9a3ec2f715852cfaca808c0c69e6ba
Vincent Gabb <60416073+gabbvincent@users.noreply.github.com>
2020-04-07 20:03:24 -0700

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ package main
 import "fmt"
 
 func swap(a, b int){
+  //Print a and b, swap their values, then print them again.
   fmt.Println(a, b)
   var hold = a
   a = b
@@ -16,11 +17,14 @@ func swap(a, b int){
 }
 
 func main() {
+  //declare variables for a and b as integers
   var a int 
   var b int
+  //ask user for 2 numbers, scan for a for the first, and for b for the second.
   fmt.Println("enter a number")
   fmt.Scanln(&a)
   fmt.Println("enter in another number")
   fmt.Scanln(&b)
+  //call the function swap(a, b)
    swap(a, b)
 }
